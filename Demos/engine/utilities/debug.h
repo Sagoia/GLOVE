a377f009c62a6b6fd8f1d1cef1bc6e30f9090d28
Kostas Vardis <k.vardis@think-silicon.com>
2018-09-21 13:10:33 +0300

    Added meaningfull print messages on glError

--- Demos/engine/utilities/debug.h
+++ Demos/engine/utilities/debug.h
@@ -24,7 +24,26 @@
 #include <GLES2/gl2ext.h>
 
 #ifdef DEBUG_OPENGLES
-#define ASSERT_NO_GL_ERROR() { GLenum err = glGetError(); assert(err == GL_NO_ERROR); }
+#define ASSERT_NO_GL_ERROR() \
+{   GLenum err = glGetError(); \
+    switch (err) {\
+    case GL_INVALID_ENUM:\
+        printf("Error GL_INVALID_ENUM. An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag.\n");\
+        break;\
+    case GL_INVALID_VALUE:\
+        printf("Error GL_INVALID_VALUE. A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag.\n");\
+        break;\
+    case GL_INVALID_OPERATION:\
+        printf("Error GL_INVALID_OPERATION. The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag.\n");\
+        break;\
+    case GL_INVALID_FRAMEBUFFER_OPERATION:\
+        printf("Error GL_INVALID_FRAMEBUFFER_OPERATION. The command is trying to render to or read from the framebuffer while the currently bound framebuffer is not framebuffer complete (i.e. the return value from glCheckFramebufferStatus is not GL_FRAMEBUFFER_COMPLETE). The offending command is ignored and has no other side effect than to set the error flag.\n");\
+        break;\
+    case GL_OUT_OF_MEMORY:\
+        printf("Error GL_OUT_OF_MEMORY. There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded.\n");\
+        break;\
+    }\
+}
 #else
 #define ASSERT_NO_GL_ERROR()
 #endif
