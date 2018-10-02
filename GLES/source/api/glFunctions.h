d9d1d9c7778d900ca0a7319ff97823164a751365
Kostas Vardis <k.vardis@think-silicon.com>
2018-10-02 11:47:24 +0300

    Added missing includes from header files for Android compilation

--- GLES/source/api/glFunctions.h
+++ GLES/source/api/glFunctions.h
@@ -4,6 +4,7 @@
 
 #include "GLES2/gl2.h"
 #include "GLES2/gl2ext.h"
+#include <string>
 #include <unordered_map>
 static const std::unordered_map<std::string, GLPROC> glFPMap = {
 GL_FUNC_PTR(glActiveTexture),
