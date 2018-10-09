3f64f3f4aa6aaae8c5e8f79fe0480aa222fcd1b3
Kostas Vardis <k.vardis@think-silicon.com>
2018-10-09 13:12:14 +0300

    Added missing copyright in eglFunctions.h

--- EGL/source/api/eglFunctions.h
+++ EGL/source/api/eglFunctions.h
@@ -1,3 +1,26 @@
+/**
+ * Copyright (C) 2015-2018 Think Silicon S.A. (https://think-silicon.com/)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public v3
+ * License as published by the Free Software Foundation;
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
+
+/**
+ *  @file       eglFunctions.h
+ *  @author     Think Silicon
+ *  @date       28/09/2018
+ *  @version    1.0
+ *
+ *  @brief      EGL function pointers helper file
+ *
+ */
+
 #ifndef EGL_FUNC_PTR
 #define EGL_FUNC_PTR(f) { #f, reinterpret_cast<__eglMustCastToProperFunctionPointerType>(f) }
 #endif //  EGL_FUNC_PTR
