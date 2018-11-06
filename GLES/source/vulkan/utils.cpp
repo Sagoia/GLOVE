8c81d9868b136b59d2309862cae86507d03139ba
avasilakis <a.vasilakis@think-silicon.com>
2018-11-06 17:38:08 +0200

    Fix default values

--- GLES/source/vulkan/utils.cpp
+++ GLES/source/vulkan/utils.cpp
@@ -40,7 +40,7 @@ GetVkFormatDepthBits(VkFormat format)
         case VK_FORMAT_D24_UNORM_S8_UINT:       return 24;
         case VK_FORMAT_D32_SFLOAT:
         case VK_FORMAT_D32_SFLOAT_S8_UINT:      return 32;
-        default: { NOT_REACHED(); return 24; }
+        default: { NOT_REACHED();               return 0; }
     }
 }
 
@@ -55,7 +55,7 @@ GetVkFormatStencilBits(VkFormat format)
         case VK_FORMAT_D16_UNORM:
         case VK_FORMAT_X8_D24_UNORM_PACK32:
         case VK_FORMAT_D32_SFLOAT:              return 0;
-        default: { NOT_REACHED(); return 24; }
+        default: { NOT_REACHED();               return 0; }
     }
 }
 
