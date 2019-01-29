283dbdec833b18d6c759c26de430b0c7e36bfd3d
Julian <zw616636719@gmail.com>
2019-01-29 10:12:45 +0800

    Set usage flag VK_IMAGE_USAGE_SAMPLED_BIT to binded texture

--- GLES/source/context/contextTexture.cpp
+++ GLES/source/context/contextTexture.cpp
@@ -56,7 +56,7 @@ Context::BindTexture(GLenum target, GLuint texture)
             tex->SetCommandBufferManager(mCommandBufferManager);
             tex->SetCacheManager(mCacheManager);
             tex->SetTarget(target);
-            tex->SetVkImageUsage(static_cast<VkImageUsageFlagBits>(VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT));
+            tex->SetVkImageUsage(static_cast<VkImageUsageFlagBits>(VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT));
             tex->SetVkImageTarget(target == GL_TEXTURE_2D ? vulkanAPI::Image::VK_IMAGE_TARGET_2D : vulkanAPI::Image::VK_IMAGE_TARGET_CUBE);
             tex->SetVkImageTiling();
 
