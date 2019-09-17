990a081fdb479d6b4a6f3913703f77ffdb460135
d.georgakakis <d.georgakakis@think-silicon.com>
2019-09-17 15:43:21 +0300

    Allow external sources build/update in Microsoft Windows (python 3)

--- update_external_sources.py
+++ update_external_sources.py
@@ -18,24 +18,24 @@ GLSLANG_FLAGS="-DENABLE_AMD_EXTENSIONS=OFF -DENABLE_NV_EXTENSIONS=OFF -DENABLE_O
 
 def ReadLine(fileName):
     file = open(fileName, "rb")
-    text = file.readline()
+    text = str(file.readline(), 'utf-8')
     file.close()
     return text
 
 def Create(proj, repo, ver):
     projPath = EXT_DIR + os.path.sep + proj
-    print "Creating local " + proj + " repository (" + projPath + ")"
+    print("Creating local " + proj + " repository (" + projPath + ")")
     if os.path.exists(projPath):
         shutil.rmtree(projPath)
     os.mkdir(projPath)
     os.chdir(projPath)
     check_output(["git", "clone", repo, "."])
-    check_output(["git", "checkout", ver])
+    check_output(["git", "checkout", str(ver)])
     os.chdir(BASEDIR)
 
 def Update(proj, ver):
     projPath = EXT_DIR + os.path.sep + proj
-    print "Updating " + proj + " repository ("+ projPath + ")"
+    print("Updating " + proj + " repository ("+ projPath + ")")
     os.chdir(projPath)
     check_output(["git", "fetch", "--all"])
     check_output(["git", "checkout", "--force", ver])
@@ -43,7 +43,7 @@ def Update(proj, ver):
 
 def Build(proj):
     projPath = EXT_DIR + os.path.sep + proj
-    print "Building " + proj
+    print("Building " + proj)
 
     install_prefix = INSTALL_PATH
     if install_prefix == "":
@@ -58,34 +58,39 @@ def Build(proj):
     if proj == "glslang":
         project_flags = GLSLANG_FLAGS
 
-    print "PROJECT_FLAGS: " + project_flags
+    print("PROJECT_FLAGS: " + project_flags)
 
-    if sys.platform == "linux2" :
-        print check_output(["cmake", "-DCMAKE_BUILD_TYPE=Release",
+    if sys.platform == "linux2" or sys.platform == "linux":
+        print(check_output(["cmake", "-DCMAKE_BUILD_TYPE=Release",
                                      "-DCMAKE_TOOLCHAIN_FILE=" + TOOLCHAIN_FILE,
                                      "-DCMAKE_SYSROOT=" + SYSROOT,
                                      "-DCMAKE_INSTALL_PREFIX=" + install_prefix,
                                      project_flags,
-                                     ".."])
+                                     ".."]))
         os.system("make -j" + str(multiprocessing.cpu_count()))
         os.system("make install")
+    elif sys.platform == "win32" :
+        print(check_output(["cmake", "-DCMAKE_INSTALL_PREFIX=" + install_prefix,
+                                     "-G", "Visual Studio 16 2019", 
+                                     ".."]))
+        os.system("cmake --build . --config Release --target install")
     else :
-        print sys.platform + " not supported!"
+        print(sys.platform + " not supported!")
         return
 
     os.chdir(BASEDIR)
 
 def PrintUsage():
-    print 'Usage:'
-    print 'update_external_sources.py -i <install_path> -s <sysroot>'
-    print ' -i | --install-path (dir)    # set custom installation path'
-    print ' -s | --sysroot      (dir)    # set sysroot for cross compilation'
+    print('Usage:')
+    print('update_external_sources.py -i <install_path> -s <sysroot>')
+    print(' -i | --install-path (dir)    # set custom installation path')
+    print(' -s | --sysroot      (dir)    # set sysroot for cross compilation')
 
 def ParseOptions():
     try:
         opts, args = getopt.getopt(sys.argv[1:],"hi:s:",["install_path=","sysroot="])
     except getopt.GetoptError:
-        print 'Unrecognized option!'
+        print('Unrecognized option!')
         PrintUsage()
         sys.exit(2)
     for opt, arg in opts:
@@ -105,8 +110,8 @@ ParseOptions()
             
 GLSLANG_REVISION = ReadLine(EXT_DIR + os.path.sep + "glslang_revision")
 GOOGLETEST_REVISION = ReadLine(EXT_DIR + os.path.sep + "googletest_revision")
-print 'glslang revision: ' + GLSLANG_REVISION
-print 'googletest revision: ' + GOOGLETEST_REVISION
+print('glslang revision: ' + GLSLANG_REVISION)
+print('googletest revision: ' + GOOGLETEST_REVISION)
 
 if os.path.exists(EXT_DIR + os.path.sep + "glslang") == False or os.path.exists(EXT_DIR + os.path.sep + "glslang" + os.path.sep + ".git") == False :
     Create("glslang", GLSLANG_REPOSITORY, GLSLANG_REVISION)
